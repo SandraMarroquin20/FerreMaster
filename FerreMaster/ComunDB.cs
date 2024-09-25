b17a3992b28248d807885510fc1569ec7b619681
Kevin Miranda <kevinjosuemiranda25@hotmail.com>
2024-09-25 17:01:26 -0600

    Clase para la conexion de la Base de Datos

---
+++ FerreMaster/ComunDB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FerreMaster
+{
+	internal class ComunDB
+	{
+		//Esta clase es para la cadena ded conexcion de la Base de Datos
+	}
+}
