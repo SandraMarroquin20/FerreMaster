cb29a3c500d8b715d7601b1caff8eec1d4a00326
Sandra <Sara@DESKTOP-JSF8LSD>
2024-10-04 14:11:49 -0600

    Interfaz de login (Se hizo un cambio)

--- FerreMaster/InterfazUsuario/Login.Designer.cs
+++ FerreMaster/InterfazUsuario/Login.Designer.cs
@@ -28,138 +28,185 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.panel1 = new System.Windows.Forms.Panel();
-			this.panel2 = new System.Windows.Forms.Panel();
-			this.label1 = new System.Windows.Forms.Label();
-			this.label4 = new System.Windows.Forms.Label();
-			this.btnIngresar = new System.Windows.Forms.Button();
-			this.txtContraseña = new System.Windows.Forms.TextBox();
-			this.txtUsuario = new System.Windows.Forms.TextBox();
-			this.lblContraseña = new System.Windows.Forms.Label();
-			this.lblUsuario = new System.Windows.Forms.Label();
-			this.panel1.SuspendLayout();
-			this.panel2.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// panel1
-			// 
-			this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-			this.panel1.Controls.Add(this.label1);
-			this.panel1.Location = new System.Drawing.Point(1, 3);
-			this.panel1.Name = "panel1";
-			this.panel1.Size = new System.Drawing.Size(574, 123);
-			this.panel1.TabIndex = 0;
-			// 
-			// panel2
-			// 
-			this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
-			this.panel2.Controls.Add(this.btnIngresar);
-			this.panel2.Controls.Add(this.txtContraseña);
-			this.panel2.Controls.Add(this.txtUsuario);
-			this.panel2.Controls.Add(this.lblContraseña);
-			this.panel2.Controls.Add(this.lblUsuario);
-			this.panel2.Controls.Add(this.label4);
-			this.panel2.Location = new System.Drawing.Point(1, 132);
-			this.panel2.Name = "panel2";
-			this.panel2.Size = new System.Drawing.Size(574, 287);
-			this.panel2.TabIndex = 1;
-			// 
-			// label1
-			// 
-			this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
-			this.label1.AutoSize = true;
-			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(98, 21);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(381, 73);
-			this.label1.TabIndex = 1;
-			this.label1.Text = "FerreMaster";
-			// 
-			// label4
-			// 
-			this.label4.Anchor = System.Windows.Forms.AnchorStyles.Top;
-			this.label4.AutoSize = true;
-			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
-			this.label4.Location = new System.Drawing.Point(188, 9);
-			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(212, 37);
-			this.label4.TabIndex = 5;
-			this.label4.Text = "Bienvenido/a";
-			// 
-			// btnIngresar
-			// 
-			this.btnIngresar.Anchor = System.Windows.Forms.AnchorStyles.None;
-			this.btnIngresar.BackColor = System.Drawing.Color.Cyan;
-			this.btnIngresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.btnIngresar.ForeColor = System.Drawing.SystemColors.ControlText;
-			this.btnIngresar.Location = new System.Drawing.Point(195, 199);
-			this.btnIngresar.Name = "btnIngresar";
-			this.btnIngresar.Size = new System.Drawing.Size(164, 49);
-			this.btnIngresar.TabIndex = 12;
-			this.btnIngresar.Text = "Ingresar";
-			this.btnIngresar.UseVisualStyleBackColor = false;
-			// 
-			// txtContraseña
-			// 
-			this.txtContraseña.Anchor = System.Windows.Forms.AnchorStyles.None;
-			this.txtContraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.txtContraseña.Location = new System.Drawing.Point(303, 136);
-			this.txtContraseña.Name = "txtContraseña";
-			this.txtContraseña.Size = new System.Drawing.Size(178, 26);
-			this.txtContraseña.TabIndex = 11;
-			// 
-			// txtUsuario
-			// 
-			this.txtUsuario.Anchor = System.Windows.Forms.AnchorStyles.None;
-			this.txtUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.txtUsuario.Location = new System.Drawing.Point(303, 69);
-			this.txtUsuario.Name = "txtUsuario";
-			this.txtUsuario.Size = new System.Drawing.Size(178, 26);
-			this.txtUsuario.TabIndex = 10;
-			// 
-			// lblContraseña
-			// 
-			this.lblContraseña.Anchor = System.Windows.Forms.AnchorStyles.None;
-			this.lblContraseña.AutoSize = true;
-			this.lblContraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.lblContraseña.Location = new System.Drawing.Point(86, 125);
-			this.lblContraseña.Name = "lblContraseña";
-			this.lblContraseña.Size = new System.Drawing.Size(203, 37);
-			this.lblContraseña.TabIndex = 9;
-			this.lblContraseña.Text = "Contraseña:";
-			// 
-			// lblUsuario
-			// 
-			this.lblUsuario.Anchor = System.Windows.Forms.AnchorStyles.None;
-			this.lblUsuario.AutoSize = true;
-			this.lblUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
-			this.lblUsuario.Location = new System.Drawing.Point(86, 59);
-			this.lblUsuario.Name = "lblUsuario";
-			this.lblUsuario.Size = new System.Drawing.Size(145, 37);
-			this.lblUsuario.TabIndex = 8;
-			this.lblUsuario.Text = "Usuario:";
-			// 
-			// Login
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(573, 417);
-			this.Controls.Add(this.panel2);
-			this.Controls.Add(this.panel1);
-			this.Name = "Login";
-			this.Text = "Login";
-			this.panel1.ResumeLayout(false);
-			this.panel1.PerformLayout();
-			this.panel2.ResumeLayout(false);
-			this.panel2.PerformLayout();
-			this.ResumeLayout(false);
+            System.Windows.Forms.Button button1;
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.lblContraseña = new System.Windows.Forms.Label();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            button1 = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.BackgroundImage = global::FerreMaster.Properties.Resources._6843484;
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel2.Controls.Add(button1);
+            this.panel2.Controls.Add(this.btnIngresar);
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Controls.Add(this.txtContraseña);
+            this.panel2.Controls.Add(this.txtUsuario);
+            this.panel2.Controls.Add(this.lblContraseña);
+            this.panel2.Controls.Add(this.lblUsuario);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Location = new System.Drawing.Point(-1, -1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(576, 420);
+            this.panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.BackColor = System.Drawing.Color.Transparent;
+            button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            button1.ForeColor = System.Drawing.Color.Transparent;
+            button1.Image = global::FerreMaster.Properties.Resources.borrar;
+            button1.Location = new System.Drawing.Point(528, 13);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(45, 25);
+            button1.TabIndex = 13;
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnIngresar.BackColor = System.Drawing.Color.Blue;
+            this.btnIngresar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnIngresar.FlatAppearance.BorderSize = 0;
+            this.btnIngresar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnIngresar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnIngresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIngresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIngresar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnIngresar.Location = new System.Drawing.Point(209, 337);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(164, 49);
+            this.btnIngresar.TabIndex = 12;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = false;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(50, 48);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(458, 98);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(30, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(381, 73);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "FerreMaster";
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtContraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContraseña.Location = new System.Drawing.Point(268, 276);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(222, 26);
+            this.txtContraseña.TabIndex = 11;
+            this.txtContraseña.TextChanged += new System.EventHandler(this.txtContraseña_TextChanged);
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.Location = new System.Drawing.Point(268, 216);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(222, 26);
+            this.txtUsuario.TabIndex = 10;
+            this.txtUsuario.TextChanged += new System.EventHandler(this.txtUsuario_TextChanged);
+            // 
+            // lblContraseña
+            // 
+            this.lblContraseña.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblContraseña.AutoSize = true;
+            this.lblContraseña.BackColor = System.Drawing.Color.Transparent;
+            this.lblContraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContraseña.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblContraseña.Location = new System.Drawing.Point(59, 264);
+            this.lblContraseña.Name = "lblContraseña";
+            this.lblContraseña.Size = new System.Drawing.Size(203, 37);
+            this.lblContraseña.TabIndex = 9;
+            this.lblContraseña.Text = "Contraseña:";
+            this.lblContraseña.Click += new System.EventHandler(this.lblContraseña_Click);
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.BackColor = System.Drawing.Color.Transparent;
+            this.lblUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblUsuario.Location = new System.Drawing.Point(96, 206);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(145, 37);
+            this.lblUsuario.TabIndex = 8;
+            this.lblUsuario.Text = "Usuario:";
+            this.lblUsuario.Click += new System.EventHandler(this.lblUsuario_Click);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.CausesValidation = false;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(166, 159);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(214, 39);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Bienvenido/a";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.BackgroundImage = global::FerreMaster.Properties.Resources._6843484;
+            this.ClientSize = new System.Drawing.Size(573, 417);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Login";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
 
 		}
 
 		#endregion
-
-		private System.Windows.Forms.Panel panel1;
-		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Panel panel2;
 		private System.Windows.Forms.Button btnIngresar;
 		private System.Windows.Forms.TextBox txtContraseña;
@@ -167,6 +214,8 @@
 		private System.Windows.Forms.Label lblContraseña;
 		private System.Windows.Forms.Label lblUsuario;
 		private System.Windows.Forms.Label label4;
-	}
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+    }
 }
 
