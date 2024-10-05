63e6b878ba2f3f8f8b51ad68b45fadb91a357a67
SandraMarroquin20 <sandra1234jad@gmail.com>
2024-10-04 23:31:15 -0600

    modificacion a login

--- FerreMaster/InterfazUsuario/Login.cs
+++ FerreMaster/InterfazUsuario/Login.cs
@@ -1,12 +1,48 @@
-﻿using System.Windows.Forms;
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace FerreMaster
 {
 	public partial class Login : Form
 	{
-		public Login()
+
+        public Login()
 		{
 			InitializeComponent();
-		}
-	}
+            
+        }
+
+        private void txtUsuario_TextChanged(object sender, System.EventArgs e)
+        {
+        }
+
+        private void txtContraseña_TextChanged(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void btnIngresar_Click(object sender, System.EventArgs e)
+        {
+            string usuario = txtUsuario.Text;
+            string contrasena = txtContraseña.Text;
+
+ 
+        }
+
+        private void lblUsuario_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void lblContraseña_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            this.Close();
+        }
+    }
 }
